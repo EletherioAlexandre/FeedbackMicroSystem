6e7a685361ae36471e1615bb6a5f675787e1059a
GusSRod <gugast88@gmail.com>
2023-12-02 17:19:30 -0300

    Atualizando branch para receber merge

---
+++ FeedBackWhatsApp/Controllers/FeedBackWhatsAppController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+[Route("api/[controller]")]
+[ApiController]
+public class FeedBackWhastAppController : ControllerBase
+{
+    private readonly HttpClient _httpClient;
+
+    public FeedBackWhastAppController(HttpClient httpClient)
+    {
+        _httpClient = httpClient;
+    }
+
+    [HttpPost]
+    public async Task<IActionResult> Post([FromBody] string content)
+    {
+      
+        var response = await _httpClient.PostAsJsonAsync("https://sender-api.levilo.com.br/message/sendText/colocarnomedainstanciaaqui", content);
+        if (!response.IsSuccessStatusCode)
+        {
+            return StatusCode((int)response.StatusCode);
+        }
+
+        return Ok();
+    }
+}
--- FeedbackMicroSystem/Controllers/FeedbackController.cs
+++ FeedbackMicroSystem/Controllers/FeedbackController.cs
@@ -1,18 +1,1 @@
-﻿using Microsoft.AspNetCore.Mvc;
-
-// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
-
-namespace FeedbackMicroSystem.Controllers
-{
-    [Route("api/[controller]")]
-    [ApiController]
-    public class FeedbackController : ControllerBase
-    {
-        // GET: api/<FeedbackController>
-        [HttpGet]
-        public IEnumerable<string> Get()
-        {
-            return new string[] { "value1", "value2" };
-        }
-    }
-}
+﻿
